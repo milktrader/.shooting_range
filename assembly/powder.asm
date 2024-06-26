f837de3715b0dfef4a38f7ee4a5daeca5516386a
milktrader <milktrader@hushmail.com>
2024-06-26 13:18:05 -0400

    got something to work with OSX and compiling assembly

--- assembly/powder.asm
+++ assembly/powder.asm
@@ -1,23 +1,16 @@
- ;Keep your powder dry assembly program
- ;                          
- ;   $ nasm -f elf32 -o powder.o powder.asm
- ;   $ ld -m elf_i386 -o powder powder.o 
- ;   $ ./powder
+.code64
+.global _main
+.static_data
+msg: .ascii "\n Let's keep the powder dry!\n\n"
+.set msglen, (. - msg)
+.text
+_main:
+    mov $0x2000004, %rax 
+    mov $1, %rdi 
+    lea msg(%rip), %rsi 
+    mov $msglen, %rdx
+    syscall 
+    mov $0x2000001, %rax 
+    xor %rdi, %rdi 
+    syscall
 
-section        .text                   ; declare the .text section
-global         _start                  ; has to be declared for the linker (ld)
-_start:                                ; entry point for _start
-    mov edx, len                       ; "invoke" the len of the message
-    mov ecx, msg                       ; "invoke" the message itself
-
-    mov ebx, 1                         ; set the file descriptor (fd) to stdout
-
-    mov eax, 4                         ; system call for "write"   
-    int 0x80                           ; call the kernel
-
-    mov eax, 1                         ; system call for "exit"
-    int 0x80                           ; call the kernel
-
-section        .data                   ; here you declare the data
-    msg        db "Let's keep the powder dry"       ; the actual message to use
-    len        equ $ -msg              ; get the size of the message
